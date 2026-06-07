f8a5768901aa47cdab384e8d48fc8235e861fefa
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-06-08 00:56:59 +0800

    refactor: reorganize imports and update InputGroup component exports for consistency

--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import * as React from 'react'
-import { cva  } from 'class-variance-authority'
-import type {VariantProps} from 'class-variance-authority';
-
-import { cn } from '@/lib/utils'
+import type { VariantProps } from 'class-variance-authority'
+import { cva } from 'class-variance-authority'
+import type * as React from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
 
 function InputGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return (
@@ -153,7 +152,7 @@ export {
   InputGroup,
   InputGroupAddon,
   InputGroupButton,
-  InputGroupText,
   InputGroupInput,
+  InputGroupText,
   InputGroupTextarea,
 }
